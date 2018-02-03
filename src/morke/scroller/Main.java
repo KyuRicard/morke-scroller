42a0f8e0feb7d6fb6cbc79ff13fe963e2cac62c4
KyuRicard <ricardboscholivar@hotmail.com>
2018-02-03 19:51:08 +0100

    - Window creation - Add Library LWJGL3

--- src/morke/scroller/Main.java
+++ src/morke/scroller/Main.java
@@ -5,10 +5,22 @@ import morke.scroller.display.Display;
 public class Main {
 
 	public static void main(String[] args) {
-		Display disp = new Display("Test", 320, 320);	
+		// Load configuration
+		
+		// Create display
+		Display disp = new Display("Test", 1280, 720);	
+		
 		while(!disp.shouldWindowClose())
 		{
+			// Pull events
+			disp.eventHandler();
 			
+			// Update
+			
+			// Render
+			
+			// Render present
+			disp.swapBuffers();
 		}
 	}
 
--- src/morke/scroller/display/Display.java
+++ src/morke/scroller/display/Display.java
@@ -1,6 +1,11 @@
 package morke.scroller.display;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.lwjgl.glfw.GLFW;
+import org.lwjgl.system.Configuration;
 
 public class Display {
 	private String title;
@@ -12,7 +17,17 @@ public class Display {
 		this.title = title;
 		this.width = width;
 		this.height = height;
+
+		Path path = Paths.get("libs/lwjgl/");
+	    String librarypath = path.toAbsolutePath().toString();
+
+	    System.setProperty("org.lwjgl.librarypath", librarypath);
 		
+	    if(!GLFW.glfwInit())
+	    {
+	    	System.err.println("Error initializating GLFW.");
+	    }
+	    
 		GLFW.glfwDefaultWindowHints();
 		window = GLFW.glfwCreateWindow(width, height, title, 0, 0);
 		if(window == 0)
@@ -41,5 +56,15 @@ public class Display {
 	public boolean shouldWindowClose()
 	{
 		return GLFW.glfwWindowShouldClose(window);
+	}
+	
+	public void eventHandler()
+	{
+		GLFW.glfwPollEvents();
+	}
+	
+	public void swapBuffers()
+	{
+		GLFW.glfwSwapBuffers(window);
 	}
 }
